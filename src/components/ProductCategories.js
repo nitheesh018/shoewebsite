2dfc1a9c3ac6eae1b11a492f85da27c1d460f445
nitheesh018 <nitheeshb2001@gmail.com>
2024-11-01 16:37:00 -0500

    added header

--- src/components/ProductCategories.js
+++ src/components/ProductCategories.js
@@ -8,6 +8,7 @@ function ProductCategories() {
   return (
     <div>
       <h1>Product Categories</h1>
+      <h2>changes</h2>
       <ul>
         {categories.map((category, index) => (
           <li key={index}>
